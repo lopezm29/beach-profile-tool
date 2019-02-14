84474c6e151745a66b35edc0289acf5b1a86d2a6
Res Life SVC <lopezm29@southernct.edu>
2019-02-14 12:52:27 -0500

    Update urls.py

--- surveys/urls.py
+++ surveys/urls.py
@@ -9,10 +9,10 @@ urlpatterns=[
     path('surveys/view', views.survey_details, name='survey_details'),
     path('surveys/edit', views.survey_edit, name='survey_edit'),
     path('surveys/calc', views.survey_calc, name='survey_calc'),
-    path('surveys/confirm/survey_delete', views.survey_delete, name='survey_delete'),
-    path('profiles/confirm/profile_delete', views.profile_delete, name='profile_delete'),
-    path('surveys/confirm/station_delete', views.station_delete, name='station_delete'),
-    path('delete', views.delete, name='delete'),
+    path('surveys/survey_delete', views.survey_delete, name='survey_delete'),
+    path('surveys/profile_delete', views.profile_delete, name='profile_delete'),
+    path('surveys/station_delete', views.station_delete, name='station_delete'),
+    path('surveys/delete', views.delete, name='delete'),
     #path('stations/', StationView.as_view()), #Edit View
     #path('stations/add', StationCreate.as_view()), #Form Made
     #path('stations/edit', StationUpdate.as_view()),
