edb3e0a7cff0cbbf81a1ff46ee02b8667fdd9e6f
Res Life SVC <lopezm29@southernct.edu>
2019-02-14 15:19:02 -0500

    Fix redirects on delete
    Delete pages now properly pass object pk and survey pk to return to

--- surveys/views.py
+++ surveys/views.py
@@ -118,9 +118,11 @@ def survey_details(request):
 #    return render(request, 'surveys/survey_details.html', {'survey':survey, 'profiles':profiles, 'stations':stations})
 
 def survey_delete(request):
-    survey = get_object_or_404(Survey, instance_id=request.POST.get('pk'))
+    survey_pk = request.POST.get('pk')
+    survey = get_object_or_404(Survey, instance_id=survey_pk)
+    
     next_url = request.POST.get('next')
-    next_pk = request.POST.get('next_pk')
+    pk = request.POST.get('pk')
     
     if "POST" == request.method and "True" == request.POST.get('confirmed'):
         survey.delete()
@@ -133,10 +135,11 @@ def survey_delete(request):
     return render(request, 'surveys/survey_delete.html', {'survey':survey, 'pk':pk, 'next_url':next_url})
 
 def profile_delete(request):
-    profile = get_object_or_404(Profile, profile_id=request.POST.get('pk'))
-    pk = profile.pk
+    profile_pk = request.POST.get('profile_pk')
+    profile = get_object_or_404(Profile, profile_id=profile_pk)
+    
     next_url = request.POST.get('next')
-    next_pk = request.POST.get('next_pk')
+    pk = request.POST.get('pk')
 
     if "POST" == request.method and "True" == request.POST.get('confirmed'):
         profile.delete()
@@ -149,22 +152,23 @@ def profile_delete(request):
     return render(request, 'surveys/profile_delete.html', {'profile':profile, 'pk':pk, 'next_url':next_url})
 
 def station_delete(request):
-    station = get_object_or_404(Station, station_id=request.POST.get('pk'))
-    pk = station.station_id
+    station_pk = request.POST.get('pk')
+    station = get_object_or_404(Station, station_id=station_pk)
+    
     next_url = request.POST.get('next')
-    next_pk = request.POST.get('next_pk')
+    pk = request.POST.get('next_pk')
 
     if "POST" == request.method and "True" == request.POST.get('confirmed'):
         station.delete()
         
-        return render(request, next_url, {'pk':next_pk})
+        return render(request, next_url)
     
     elif "POST" == request.method and "False" == request.POST.get('confirmed'):
         
         
-        return render(request, next_url, {'pk':next_pk})
+        return render(request, next_url)
 
-    return render(request, 'surveys/station_delete.html', {'station':station, 'pk':pk, 'next_url':next_url})
+    return render(request, 'surveys/station_delete.html', {'station':station, 'station_pk':station_pk, 'next_url':next_url})
 
 def delete(request, confirmed_object):
     pk = request.POST.get('pk')
