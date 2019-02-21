90e41b5ac31d4256ad779d8a6175f86dfc68a471
lopezm29 <32684596+lopezm29@users.noreply.github.com>
2019-02-21 02:59:26 -0500

    Update calculations.py

--- surveys/calculations.py
+++ surveys/calculations.py
@@ -1,9 +1,15 @@
 def findIntercept(profile, stations, intercept_point):
     stations_found = False
     i = 0
+<<<<<<< HEAD
     while not stations_found and i < len(stations)-1:
         if i == len(stations)-1 :
             i += 1
+=======
+    while(!stations_found && i < stations.length-1):
+        if(i == stations.length-1):
+            i++
+>>>>>>> parent of e4738e5... Update calculations.py
         else:
             if intercept_point <= stations[i].true_elevation and intercept_point > stations[i + 1].true_elevation:
                 pult_index = i
@@ -29,10 +35,17 @@ def calcBeachWidth(profile, stations, intercept_point):
 def calcBeachVolume(profile, stations, intercept_point):
     pult_index, mllw = findIntercept(profile=profile, stations=stations, intercept_point=intercept_point)
     volume = 0
+<<<<<<< HEAD
     for i in range(len(stations)-1):
         if i == pult_index:
             left = stations[i].true_elevation - intercept_point
             distance = mllw - stations[i].true_distance
+=======
+    for i in range(stations.length-1):
+        if(i == pult_index):
+            left = station[i].true_elevation - intercept_point
+            distance = mllw - station[i].true_distance
+>>>>>>> parent of e4738e5... Update calculations.py
             trapezoid_area = left * distance / 2
             volume += trapezoid_area
 
