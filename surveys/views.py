b1657313e9824dc83a4bb4884f913ec8f50ddeb2
lopezm29 <32684596+lopezm29@users.noreply.github.com>
2019-02-20 22:55:22 -0500

    Update views.py

--- surveys/views.py
+++ surveys/views.py
@@ -127,12 +127,12 @@ def survey_details(request):
 
     return profiles_stations_pair
 
-def survey_details(request):
-    survey = get_object_or_404(Survey, instance_id=request.POST.get('pk'))
+# def survey_details(request):
+#     survey = get_object_or_404(Survey, instance_id=request.POST.get('pk'))
     
-    profiles_stations_pair = get_profiles_stations_pair(survey_pk=request.POST.get('pk'))
+#     profiles_stations_pair = get_profiles_stations_pair(survey_pk=request.POST.get('pk'))
 
-    return render(request, 'surveys/survey_details.html', {'survey':survey, 'profiles_stations_pair':profiles_stations_pair})
+#     return render(request, 'surveys/survey_details.html', {'survey':survey, 'profiles_stations_pair':profiles_stations_pair})
 #   if request.POST['pk']:
 #        profiles = Profile.objects.filter(survey_instance=request.POST['pk'])
 #        #get_list_or_404(Profile, survey_instance=request.POST['pk'])
