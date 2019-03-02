e1d4bb775caf80a31e0051065157340fea450e40
lopezm29 <32684596+lopezm29@users.noreply.github.com>
2019-03-02 04:25:47 -0500

    Update forms.py

--- surveys/forms.py
+++ surveys/forms.py
@@ -4,19 +4,14 @@ from .models import *
 class SurveyCreate(forms.ModelForm):
     class Meta():
         model = Survey
-        exclude = ['width']
+        fields = '__all__'
 
 class ProfileCreate(forms.ModelForm):
     class Meta():
         model = Profile
         exclude = ['volume', 'width']
 
-class StationEdit(forms.ModelForm):
-    class Meta():
-        model = Station
-        fields = '__all__'
-
 class StationCreate(forms.ModelForm):
     class Meta():
         model = Station
-        fields = '__all__'
+        exclude = ['true_distance', 'true_elevation']
